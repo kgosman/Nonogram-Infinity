acadb75dba100e8cd1cf3bcbc2513343cc97f667
niccschmitt <40522061+niccschmitt@users.noreply.github.com>
2019-11-06 20:44:30 -0500

    stuff

--- Classes/Member.cs
+++ Classes/Member.cs
@@ -4,49 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _5TSPGA
+namespace Nonogram_Infinity
 {
     public class Member
     {
-        public bool expert;
-        public double fitness;
-        public List<City> dna;
-        public Member ()
-        {
-            this.expert = false;
-            this.fitness = 0.0;
-            this.dna = new List<City>();
-        }
-        //Calculate fitness of the dna
-        public void FindFitness()
+        public int fitness;
+        public bool[,] dna;
+        private int row;
+        private int col;
+        public Member (int row, int col, int black_squares)
         {
             this.fitness = 0;
-            for(int i = 0; i+1 < dna.Count; i++)
+            this.dna = new bool[row, col];
+            this.row = row;
+            this.col = col;
+            int l, k, rng;
+            int[] array = new int[row * col];
+            for (int i = 0; i < row; i++)
             {
-                dna[i].DistanceToCity(dna[i + 1]);
-                fitness += dna[i].distance;
+                for (int j = 0; j < col; j++)
+                {
+                    dna[i, j] = false;
+                    array[col * i + j] = col * i + j;
+                }
             }
+            int tmp;
+            for (int i = 0; i < black_squares; i++)
+            {
+                rng = RandomHolder.Instance.Next(0, row * col-i);
+                tmp = array[rng];
+                array[rng] = array[row * col - i - 1];
+                array[row * col - i - 1] = tmp;
+                l = tmp / col;
+                k = tmp % col;
+                dna[l, k] = true;
+
+            }
+            FindFitness();
+        }
+        //Calculate fitness of the dna
+        public void FindFitness() //Will todo
+        {
+           
         }
         //Clone a members dna
-        public void Clone(List<City> myCities)
+        public void Clone(bool[,] copyDNA)
         {
-            foreach(City city in myCities)
-            {
-                dna.Add(city);
-            }
+            dna = copyDNA;
         }
-        //Shuffle the DNA
-        public void RandomizeDNA()
-        {
-            int i = dna.Count;
-            while (i > 1)
-            {
-                i--;
-                int j = RandomHolder.Instance.Next(i + 1);
-                City tmpCity = dna[j];
-                dna[j] = dna[i];
-                dna[i] = tmpCity;
-            }
-        }
+        
     }
 }
