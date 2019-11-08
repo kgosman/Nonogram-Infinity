552c45e4fe97131d33c34d2e5215a4b806c816bf
Will Crawford <williambcrawf@gmail.com>
2019-11-08 13:24:59 -0500

    Mutation done

--- Classes/Member.cs
+++ Classes/Member.cs
@@ -42,39 +42,61 @@ namespace Nonogram_Infinity
 
             ColumnFitness = new List<int>(Column);
 
+            this.DNA = new bool[this.Row, this.Column];
 
-                this.DNA = new bool[this.Row, this.Column];
-
-
-                int l, k, rng;
-                int[] array = new int[Row * Column];
-                for (int i = 0; i < Row; i++)
+            int l, k, rng;
+            int[] array = new int[Row * Column];
+            for (int i = 0; i < Row; i++)
+            {
+                for (int j = 0; j < Column; j++)
                 {
-                    for (int j = 0; j < Column; j++)
-                    {
-                        DNA[i, j] = false;
-                        array[Column * i + j] = Column * i + j;
-                    }
+                    DNA[i, j] = false;
+                    array[Column * i + j] = Column * i + j;
                 }
-                int tmp;
-                for (int i = 0; i < BlackSquares; i++)
-                {
-                    rng = RandomHolder.Instance.Next(0, Row * Column - i);
-                    tmp = array[rng];
-                    array[rng] = array[Row * Column - i - 1];
-                    array[Row * Column - i - 1] = tmp;
-                    l = tmp / Column;
-                    k = tmp % Column;
-                    DNA[l, k] = true;
+            }
+            int tmp;
+            for (int i = 0; i < BlackSquares; i++)
+            {
+                rng = RandomHolder.Instance.Next(0, Row * Column - i);
+                tmp = array[rng];
+                array[rng] = array[Row * Column - i - 1];
+                array[Row * Column - i - 1] = tmp;
+                l = tmp / Column;
+                k = tmp % Column;
+                DNA[l, k] = true;
 
-                }
-
+            }
 
             FindFitness(RowRules, ColumnRules);
         }
-        /*
-         * Calculates the distance of the DNA
-         */
+
+        public Member(int Row, int Column, int BlackSquares, List<int>[] RowRules, List<int>[] ColumnRules, bool[,] DNA)
+        {
+            Fitness = 0;
+            this.Row = Row;
+            this.Column = Column;
+
+            RowFitness = new List<int>(Row);
+
+            ColumnFitness = new List<int>(Column);
+
+            this.DNA = DNA;
+
+            FindFitness(RowRules, ColumnRules);
+        }
+
+        /// <summary>
+        /// Calculates the fitness of a given member,
+        /// this calculation is done by determining
+        /// if the runs of black squares adhere to
+        /// the <paramref name="ColumnRules"/> and
+        /// <paramref name="RowRules"/>. It also
+        /// will take into account whether or not the
+        /// total number of black squares adhere
+        /// to the rules provided.
+        /// </summary>
+        /// <param name="RowRules"></param>
+        /// <param name="ColumnRules"></param>
         public void FindFitness(List<int>[] RowRules, List<int>[] ColumnRules) //Will todo
         {
             RowWiseFitness(RowRules);
@@ -87,7 +109,7 @@ namespace Nonogram_Infinity
                 Fitness += fitness;
         }
 
-        public void RowWiseFitness(List<int>[] RowRules)
+        private void RowWiseFitness(List<int>[] RowRules)
         {
             int fitness = 0;
 
@@ -137,7 +159,7 @@ namespace Nonogram_Infinity
             }
         }
 
-        public void ColumnWiseFitness(List<int>[] ColumnRules)
+        private void ColumnWiseFitness(List<int>[] ColumnRules)
         {
             int fitness = 0;
 
@@ -188,10 +210,95 @@ namespace Nonogram_Infinity
             }
         }
 
-        //Clone a members dna
+        /// <summary>
+        /// Copies input into DNA
+        /// </summary>
+        /// <param name="copyDNA"></param>
         public void Clone(bool[,] copyDNA)
         {
             DNA = copyDNA;
+        }
+
+         ///<summary>
+         ///Mutates a given member by columns or rows
+         ///depending on the population and if a given
+         ///row or column is at a fitness = 0;
+         ///</summary>
+        internal void Mutate(bool rowWise, bool columnWise)
+        {
+            if (rowWise)
+            {
+                int row1Index = 0, row2Index = 1;
+
+                bool row1Okay = false, row2Okay = false;
+
+                while (true)
+                {
+                    if (!row1Okay) row1Index = RandomHolder.Instance.Next(0, Row);
+
+                    if (!row2Okay) row2Index = RandomHolder.Instance.Next(0, Row);
+
+                    if (RowFitness[row1Index] > 0) row1Okay = true;
+
+                    if (RowFitness[row2Index] > 0) row2Okay = true;
+
+                    if (row1Index == row2Index) row1Okay = row2Okay = false;
+
+                    if (row1Okay && row2Okay) break;
+                }
+
+                bool[] row1;
+                bool[] row2 = row1 = new bool[Column];
+
+                for (int i = 0; i < Column; i++)
+                {
+                    row1[i] = DNA[row1Index, i];
+                    row2[i] = DNA[row2Index, i];
+                }
+
+                for (int i = 0; i < Column; i++)
+                {
+                    DNA[row1Index, i] = row2[i];
+                    DNA[row2Index, i] = row1[i];
+                }
+            }
+
+            if (columnWise)
+            {
+                int col1Index = 0, col2Index = 1;
+
+                bool col1Okay = false, col2Okay = false;
+
+                while (true)
+                {
+                    if (!col1Okay) col1Index = RandomHolder.Instance.Next(0, Column);
+
+                    if (!col2Okay) col2Index = RandomHolder.Instance.Next(0, Column);
+
+                    if (ColumnFitness[col1Index] > 0) col1Okay = true;
+
+                    if (ColumnFitness[col2Index] > 0) col2Okay = true;
+
+                    if (col1Index == col2Index) col1Okay = col2Okay = false;
+
+                    if (col1Okay && col2Okay) break;
+                }
+
+                bool[] col1;
+                bool[] col2 = col1 = new bool[Row];
+
+                for (int i = 0; i < Row; i++)
+                {
+                    col1[i] = DNA[i, col1Index];
+                    col2[i] = DNA[i, col2Index];
+                }
+
+                for (int i = 0; i < Row; i++)
+                {
+                    DNA[i, col1Index] = col2[i];
+                    DNA[i, col2Index] = col1[i];
+                }
+            }
         }
     }
 
